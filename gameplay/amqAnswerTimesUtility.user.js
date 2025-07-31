d25607cd7550b0e10ca797a028794447ca8d116b
kempanator <1497607+kempanator@users.noreply.github.com>
2025-07-31 15:10:20 -0500

    Update amqAnswerTimesUtility.user.js

--- gameplay/amqAnswerTimesUtility.user.js
+++ gameplay/amqAnswerTimesUtility.user.js
@@ -4,37 +4,20 @@
 
 const amqAnswerTimesUtility = new function () {
     "use strict"
-    this.songStartTime = 0
     this.playerTimes = []
     if (typeof Listener === "undefined") {
         return
     }
     
     new Listener("play next song", () => {
-        this.songStartTime = Date.now()
         this.playerTimes = []
     }).bindListener()
 
     new Listener("player answered", (data) => {
-        const time = Date.now() - this.songStartTime
         for (const item of data) {
             for (const id of item.gamePlayerIds) {
-                this.playerTimes[id] = time
+                this.playerTimes[id] = item.answerTime * 1000
             }
-        }
-    }).bindListener()
-
-    new Listener("Join Game", (data) => {
-        const quizState = data.quizState
-        if (quizState) {
-            this.songStartTime = Date.now() - quizState.songTimer * 1000
-        }
-    }).bindListener()
-
-    new Listener("Spectate Game", (data) => {
-        const quizState = data.quizState
-        if (quizState) {
-            this.songStartTime = Date.now() - quizState.songTimer * 1000
         }
     }).bindListener()
 }()
